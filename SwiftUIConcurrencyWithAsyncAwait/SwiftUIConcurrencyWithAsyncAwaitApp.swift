c7871c0f48c93bdc1ec0577a9e928edb43443b37
Amit Saini <post2amitsaini@gmail.com>
2023-10-04 17:09:30 +0530

    Initial Commit

---
+++ SwiftUIConcurrencyWithAsyncAwait/SwiftUIConcurrencyWithAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIConcurrencyWithAsyncAwaitApp.swift
+//  SwiftUIConcurrencyWithAsyncAwait
+//
+//  Created by Amit Saini on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIConcurrencyWithAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
