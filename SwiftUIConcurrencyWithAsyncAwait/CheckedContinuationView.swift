cbf9c10b9b244ad084f1cd4a97fe7ad9170f809f
Amit Saini <post2amitsaini@gmail.com>
2023-10-06 17:59:44 +0530

    CheckedContinuationView Added

---
+++ SwiftUIConcurrencyWithAsyncAwait/CheckedContinuationView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckedContinuationView.swift
+//  SwiftUIConcurrencyWithAsyncAwait
+//
+//  Created by Amit Saini on 06/10/23.
+//
+
+import SwiftUI
+
+struct CheckedContinuationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckedContinuationView()
+}
--- SwiftUIConcurrencyWithAsyncAwait/SwiftUIConcurrencyWithAsyncAwaitApp.swift
+++ SwiftUIConcurrencyWithAsyncAwait/SwiftUIConcurrencyWithAsyncAwaitApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUIConcurrencyWithAsyncAwaitApp: App {
     var body: some Scene {
         WindowGroup {
-            TaskGroupView()
+            CheckedContinuationView()
         }
     }
 }
