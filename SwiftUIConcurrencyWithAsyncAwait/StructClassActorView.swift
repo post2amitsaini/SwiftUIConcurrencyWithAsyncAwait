967561817334085ae53be8a860ce2b88dea93275
Amit Saini <post2amitsaini@gmail.com>
2023-10-08 11:53:12 +0530

    Struct Class Actor View Added

---
+++ SwiftUIConcurrencyWithAsyncAwait/StructClassActorView.swift
@@ -0,0 +1,18 @@
+//
+//  StructClassActorView.swift
+//  SwiftUIConcurrencyWithAsyncAwait
+//
+//  Created by Amit Saini on 08/10/23.
+//
+
+import SwiftUI
+
+struct StructClassActorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StructClassActorView()
+}
