a873bcabb2248d30b097fe572bf9b61bdce3c14c
Amit Saini <post2amitsaini@gmail.com>
2023-10-09 12:44:59 +0530

    Add documentation for sendable protocol

---
+++ SwiftUIConcurrencyWithAsyncAwait/SendableView.swift
@@ -0,0 +1,18 @@
+//
+//  SendableView.swift
+//  SwiftUIConcurrencyWithAsyncAwait
+//
+//  Created by Amit Saini on 09/10/23.
+//
+
+import SwiftUI
+
+struct SendableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SendableView()
+}
